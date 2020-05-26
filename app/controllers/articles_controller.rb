2e47c981c605607a8952269b63b8919aa8918ef5
imahnama <muannama25@gmail.com>
2020-05-26 18:15:35 +0300

    Add destroy method and link to delete article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,27 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
-
   end
 
   def new
     @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to article_path()
   end
 
 end
