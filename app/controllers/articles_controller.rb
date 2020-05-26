315beab5a7e44f99c27ac1e01ce0b815c43c98b1
evansinho <igiri.evanson@gmail.com>
2020-05-26 16:48:26 +0100

    create edit view and controller'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,11 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    redirect_to article_path()
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
 end
