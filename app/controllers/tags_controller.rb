a986cf8eb779eb4ae2b5418824e4c38cd8072cb7
imahnama <muannama25@gmail.com>
2020-05-27 13:41:49 +0300

    add a destroy method

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,17 @@
+class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    flash.notice = "Tag'#{@tag.name}' Destroyed!"
+    redirect_to tags_path
+  end
+
+end
